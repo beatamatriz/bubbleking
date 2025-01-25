a981db625cd8c354d279e29d1948982eba7518d0
Beatriz <beatamatriz@gmail.com>
2025-01-25 17:45:08 +0100

    speech bubbles

---
+++ scenes/3_SpeechBubbles/speech_bubbles.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var GAME_TIMER = 5
+@export var BUBBLE_COUNT = 1
+
+var count = 0
+var success = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if count >= BUBBLE_COUNT:
+		success = true
+
+func begin():
+	for speech_bubble in $SpeechBubbleManager.get_children():
+		speech_bubble.locked = false
+	$Timer.start(GAME_TIMER)
+	
+func _on_timer_timeout() -> void:
+	if success:
+		print_debug("success!")
+		get_parent().next("Success")
+	else:
+		print_debug("failure..")
+		get_parent().next("Failure")
