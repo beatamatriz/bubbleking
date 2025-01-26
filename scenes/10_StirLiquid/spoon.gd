311d26527cc4f6945dfc2f98fb5b04654ad45de2
k1hnr <k1hnr0000@gmail.com>
2025-01-26 14:11:31 +0100

    spoon update

--- scenes/10_StirLiquid/spoon.gd
+++ scenes/10_StirLiquid/spoon.gd
@@ -1,8 +1,9 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-const SPEED = 500
+const SPEED = 100
 
-func _process(delta):
-	var vec = get_viewport().get_mouse_position() - self.position # getting the vector from self to the mouse
-	vec = vec.normalized() * delta * SPEED # normalize it and multiply by time and speed
-	position += vec # move by that vector
+func _physics_process(delta):
+	if get_parent().active:
+		var vec = get_parent().get_global_mouse_position() - global_position # getting the vector from self to the mouse
+		velocity = vec*SPEED
+		move_and_slide()
