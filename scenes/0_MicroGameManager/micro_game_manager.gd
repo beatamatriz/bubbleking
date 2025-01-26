d60d998295d9198984b35c4183284699074266ee
k1hnr <k1hnr0000@gmail.com>
2025-01-26 03:18:21 +0100

    musiquita owo

--- scenes/0_MicroGameManager/micro_game_manager.gd
+++ scenes/0_MicroGameManager/micro_game_manager.gd
@@ -8,6 +8,8 @@ var mgl = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] # MicroGame List
 
 var current_mg = 0
 
+var ost_volume = -5
+
 func _ready() -> void:
 	initialize()
 
@@ -22,6 +24,7 @@ func initialize():
 	next("Default")
 
 func next(state):
+	mute(current_mg)
 	current_mg += 1
 	if current_mg >= mgl.size():
 		state = "Boss"
@@ -40,39 +43,78 @@ func next(state):
 		$Camera/Failure.visible = true
 		$TransitionTimer.start(TRANSITION_TIME)
 	$Camera.position = Vector2(0, 0)
+	
+func mute(mg: int):
+	if mgl[mg] == 1:
+		$Camera/layer1.volume_db = -80
+	if mgl[mg] == 2:
+		$Camera/layer2.volume_db = -80
+	if mgl[mg] == 3:
+		$Camera/layer3.volume_db = -80
+	if mgl[mg] == 4:
+		$Camera/layer4.volume_db = -80
+	if mgl[mg] == 5:
+		$Camera/layer5.volume_db = -80
+	if mgl[mg] == 6:
+		$Camera/layer6.volume_db = -80
+	if mgl[mg] == 7:
+		$Camera/layer7.volume_db = -80
+	if mgl[mg] == 8:
+		$Camera/layer8.volume_db = -80
+	if mgl[mg] == 9:
+		$Camera/layer9.volume_db = -80
+	if mgl[current_mg] == 10:
+		$Camera/layer10.volume_db = -80
+	if mgl[mg] == 11:
+		$Camera/layer11.volume_db = -80
+	if mgl[mg] == 12:
+		$Camera/layer12.volume_db = -80
+	
 
 func _on_transition_timer_timeout() -> void:
 	if mgl[current_mg] == 1:
+		$Camera/layer1.volume_db = ost_volume
 		next("Default")
 	if mgl[current_mg] == 2:
+		$Camera/layer2.volume_db = ost_volume
 		$Camera.position = $Two.position
 		$Two.begin()
 	if mgl[current_mg] == 3:
+		$Camera/layer3.volume_db = ost_volume
 		$Camera.position = $Three.position
 		$Three.begin()
 	if mgl[current_mg] == 4:
+		$Camera/layer4.volume_db = ost_volume
 		$Camera.position = $Four.position
 		$Four.begin()
 	if mgl[current_mg] == 5:
+		$Camera/layer5.volume_db = ost_volume
 		$Camera.position = $Five.position
 		$Five.begin()
 	if mgl[current_mg] == 6:
+		$Camera/layer6.volume_db = ost_volume
 		$Camera.position = $Six.position
 		$Six.begin()
 	if mgl[current_mg] == 7:
+		$Camera/layer7.volume_db = ost_volume
 		$Camera.position = $Seven.position
 		$Seven.begin()
 	if mgl[current_mg] == 8:
+		$Camera/layer8.volume_db = ost_volume
 		next("Default")
 	if mgl[current_mg] == 9:
+		$Camera/layer9.volume_db = ost_volume
 		$Camera.position = $Nine.position
 		$Nine.begin()
 	if mgl[current_mg] == 10:
+		$Camera/layer10.volume_db = ost_volume
 		next("Default")
 	if mgl[current_mg] == 11:
+		$Camera/layer11.volume_db = ost_volume
 		$Camera.position = $Eleven.position
 		$Eleven.begin()
 	if mgl[current_mg] == 12:
+		$Camera/layer12.volume_db = ost_volume
 		next("Default")
 	$Camera/Success.visible = false
 	$Camera/Failure.visible = false
